10c65446c849317f60c74d9be57138810bd7e164
Agustin Falco <afalco@sophilabs.com>
2016-09-18 17:40:59 -0300

    Moved previous logic from application.js to welcome.js handler.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,12 @@
-$(document).ready(function() {
-  function showSignUp() {
-    console.log('algo');
+var menuSelections = ['#homeMenu', '#logInMenu', '#createUserMenu'];
 
+function showSelectedMenu(selectedMenu) {
+  for (var i = 0; i < menuSelections.length; i++) {
+    var current = menuSelections[i];
+    if (selectedMenu == current) {
+      $(current).fadeIn();
+    }else {
+      $(current).fadeOut();
+    }
   }
-
-});
-function showSignUp() {
-  console.log('algo');
 }

3fb378123085ed3237bc0580fafbf951104f86c3
Agustin Falco <afalco@sophilabs.com>
2016-10-09 19:51:11 -0300

    Added user model reference and linked devise to it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :articles
-  resources :users
-  post  '/users/login',  to: 'users#login'
+  #resources :users
+  #post  '/users/login',  to: 'users#login'
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.

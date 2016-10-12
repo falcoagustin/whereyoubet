d4fd9c01f612779823767e6280d825ca59a5f9dc
Agustin Falco <afalco@sophilabs.com>
2016-10-12 08:24:27 -0300

    Added destroy and update methods for teams controller.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,17 +1,32 @@
 class TeamsController < ApplicationController
   def new
     @team = Team.new
+    @teams = Team.all
   end
 
   def create
     @team = Team.new(team_params)
     if @team.save
-      redirect_to @team
-    else
+      @teams = Team.all
       render 'new'
     end
   end
 
+  def update
+    @team = Team.find(params[:id])
+
+    if @team.update(team_params)
+      redirect_to 'new'
+    # else
+    #   render 'edit'
+    end
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to 'new'
+  end
   private
     def team_params
       params.require(:team).permit(:name, :country_id)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920022111) do
+ActiveRecord::Schema.define(version: 20161009213938) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"

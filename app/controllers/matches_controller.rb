6671a3cddfa6a6a930372c0ea8aeba1567c2817a
Agustin Falco <afalco@sophilabs.com>
2016-10-15 23:56:18 -0300

    Added creating matches in controller.

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,19 @@
 class MatchesController < ApplicationController
   def index
-    @mathes = Match.all
+    @matches = Match.all
+    puts @matches
   end
+
+  def create
+    @match = Match.new(team_params)
+    if @match.save
+      redirect_to :back
+    end
+  end
+
+  private
+    def team_params
+      params.require(:match).permit(:time, :local_ratio, :visitor_ratio,
+        :tie_ratio, :local_team_id, :visitor_team_id)
+    end
 end

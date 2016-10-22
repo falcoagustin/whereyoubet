d86bc765223af7d784105f3624caf308befb460a
Agustin Falco <afalco@sophilabs.com>
2016-10-22 17:43:58 -0300

    Changed name of top bets and modified header styles from home.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @top_5_matches = Match.last(5)
-    @first_5_matches = Match.first(5)
+    @best_five_matches = Match.last(5)
+    @hot_five_matches = Match.first(5)
   end
 end

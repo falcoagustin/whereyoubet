cc53a810ae6e3844f260cb410ee87b631464c581
Agustin Falco <afalco@sophilabs.com>
2016-10-21 23:20:18 -0300

    Generated migrations for users avatar.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019024840) do
+ActiveRecord::Schema.define(version: 20161022020241) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -87,6 +87,10 @@ ActiveRecord::Schema.define(version: 20161019024840) do
     t.datetime "birthday"
     t.string   "address"
     t.integer  "phone_number"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

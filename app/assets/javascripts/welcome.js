76cf36d706fa4036546871e1275a5c6c10b5ffbd
Agustin Falco <afalco@sophilabs.com>
2016-10-22 19:42:22 -0300

    Added side form to do complete bets, append algorithm in javascript and styles for it.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -20,24 +20,40 @@ function hideAccountsDropdown() {
   $('#accountsDropdown').css('display', 'none');
 }
 
-function toggleBet(matchId, buttonDOM) {
+function toggleBet(match, buttonDOM) {
   button = $(buttonDOM);
   var length = betIds.length;
+  var matchId = match.id;
   var index = betIds.indexOf(matchId);
   if (length == 3 && index == -1) return;
   if (length == 3 && index != -1) {
-    betIds.pop(matchId);
+    //betIds.pop(matchId);
+    removeMatch(matchId);
     toggleBetButton(button);
     return;
   }
   if (length < 3){
     if (index == -1) {
-      betIds.push(matchId);
+      addMatch(match);
+      //betIds.push(matchId);
     }else {
-      betIds.pop(matchId);
+      //betIds.pop(matchId);
+      removeMatch(matchId);
     }
     toggleBetButton(button);
   }
+}
+
+function addMatch(match) {
+  betIds.push(match.id);
+  var form = $('#completeBetForm').append(
+    '<div class="user-bet-row" id="match'+ match.id + '">' +
+    '<label>' + match.local_ratio + '</label>' +
+    '<label>' + match.visitor_ratio + '</label>' +
+    '<label>' + match.tie_ratio + '</label>' +
+    '<select> <option value="L"> Local </option> <option value="V"> Visitor </option> <option value="T"> Tie </option></select>' +
+    '<div>' + match.name + '</div>'
+  )
 }
 
 function toggleBetButton(button) {

3a426672ccd44d2fc1adaef5dacfc97c17f4933f
Agustin Falco <afalco@sophilabs.com>
2016-10-22 21:20:04 -0300

    Added routes for complete bets creation.

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/users/edit_password', to: 'users#edit_password'
   resources :users
   root 'welcome#index'
+  resources :complete_bets
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

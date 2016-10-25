82e3b8790b317659df5d2908058cd69cd413e7e2
Agustin Falco <afalco@sophilabs.com>
2016-10-24 23:06:23 -0300

    Modified controller to ask for empty amounts instead of nil to avoid a corrupt db insertion.

--- app/controllers/complete_bets_controller.rb
+++ app/controllers/complete_bets_controller.rb
@@ -1,12 +1,13 @@
 class CompleteBetsController < ApplicationController
-  #before_action :user_signed_in?, :authenticate_user!
+  before_action :user_signed_in?, :authenticate_user!
 
   def create
+    byebug
     complete_bet_params
     bet_object = params[:completeBet]
     amount = bet_object[:amount]
     bet_object = bet_object.except(:amount)
-    if bet_object.empty? || amount.nil?
+    if bet_object.empty? || amount.empty?
       redirect_to :back
     else
       validate_bets(bet_object, amount)

57546f1421e903d00efad034155d37aa6bb853ac
Agustin Falco <afalco@sophilabs.com>
2016-10-25 13:48:40 -0300

    Modified complete_bet controller method to catch exception.

--- app/controllers/complete_bets_controller.rb
+++ app/controllers/complete_bets_controller.rb
@@ -1,6 +1,7 @@
 class CompleteBetsController < ApplicationController
   before_action :user_signed_in?, :authenticate_user!
 
+  # TODO: handle errors
   def create
     byebug
     complete_bet_params
@@ -8,56 +9,62 @@ class CompleteBetsController < ApplicationController
     amount = bet_object[:amount]
     bet_object = bet_object.except(:amount)
     if bet_object.empty? || amount.empty?
+      return redirect_to :back
+    end
+    if !bets_are_valid(bet_object, amount)
+      return redirect_to :back
+    end
+    @complete_bet = CompleteBet.new(
+      :first_bet => @first_bet,
+      :second_bet => @second_bet,
+      :third_bet => @third_bet,
+      :amount => amount
+    )
+    if @complete_bet.save
       redirect_to :back
-    else
-      validate_bets(bet_object, amount)
-      @complete_bet = CompleteBet.new(
-        :first_bet => @first_bet,
-        :second_bet => @second_bet,
-        :third_bet => @third_bet,
-        :amount => amount
-      )
-      if @complete_bet.save
-        redirect_to :back
-      end
     end
   end
 
   private
-    def validate_bets(bet_object, amount)
-      now = DateTime.now
-      if bet_object[:id1]
-        @first_bet = Bet.new(
-          :match_id => bet_object[:id1],
-          bet_object[:select1] => true,
-          :user_id => current_user.id,
-          :executed => now,
-          :amount => amount
-        )
-        @first_bet.save
+    def bets_are_valid(bet_object, amount)
+      begin
+        now = DateTime.now
+        if bet_object[:id1]
+          @first_bet = Bet.new(
+            :match_id => bet_object[:id1],
+            bet_object[:select1] => true,
+            :user_id => current_user.id,
+            :executed => now,
+            :amount => amount
+          )
+          @first_bet.save
+        end
+        if bet_object[:id2]
+          @second_bet = Bet.new(
+            :match_id => bet_object[:id2],
+            bet_object[:select2] => true,
+            :user_id => current_user.id,
+            :executed => now,
+            :amount => amount
+          )
+          @second_bet.save
+        end
+        if bet_object[:id3]
+          @third_bet = Bet.new(
+            :match_id => bet_object[:id3],
+            bet_object[:select3] => true,
+            :user_id => current_user.id,
+            :executed => now,
+            :amount => amount
+          )
+          @third_bet.save
+        end
+        return true
+      rescue ActiveRecord::RecordInvalid => invalid
+        return false
       end
-      if bet_object[:id2]
-        @second_bet = Bet.new(
-          :match_id => bet_object[:id2],
-          bet_object[:select2] => true,
-          :user_id => current_user.id,
-          :executed => now,
-          :amount => amount
-        )
-        @second_bet.save
-      end
-      if bet_object[:id3]
-        @third_bet = Bet.new(
-          :match_id => bet_object[:id3],
-          bet_object[:select3] => true,
-          :user_id => current_user.id,
-          :executed => now,
-          :amount => amount
-        )
-        @third_bet.save
-      end
-      #return @first_bet, @second_bet, @third_bet
     end
+
     def complete_bet_params
       params.require(:completeBet).permit(
         :id1, :id2, :id3, :select1, :select2, :select3

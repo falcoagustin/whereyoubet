d7eefecc743edc56d9c6882463120d0b3641e5b1
Agustin Falco <afalco@sophilabs.com>
2016-10-25 15:06:34 -0300

    Changed Schema from migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -102,9 +102,9 @@ ActiveRecord::Schema.define(version: 20161023223516) do
 
   add_foreign_key "bets", "matches"
   add_foreign_key "bets", "users"
-  add_foreign_key "complete_bets", "matches", column: "first_bet_id"
-  add_foreign_key "complete_bets", "matches", column: "second_bet_id"
-  add_foreign_key "complete_bets", "matches", column: "third_bet_id"
+  add_foreign_key "complete_bets", "bets", column: "first_bet_id"
+  add_foreign_key "complete_bets", "bets", column: "second_bet_id"
+  add_foreign_key "complete_bets", "bets", column: "third_bet_id"
   add_foreign_key "matches", "teams", column: "local_team_id"
   add_foreign_key "matches", "teams", column: "visitor_team_id"
   add_foreign_key "teams", "countries"

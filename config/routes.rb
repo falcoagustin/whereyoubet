d6a9574c95297c2f2ecbe8cda7f989ead21928ba
Agustin Falco <afalco@sophilabs.com>
2016-11-25 04:01:43 -0300

    Changed routes from editing profile.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     collection {post :import}
   end
 
-  get '/users',  to: 'users#edit_profile'
+  get '/users/edit_profile',  to: 'users#edit_profile'
   get '/users/edit_password', to: 'users#edit_password'
   get '/users/bet_history', to: 'users#bet_history'
   get '/allbets', to: 'welcome#all_bets'
